b8e883e8f825b77caa8bf96ec3c0d6775f4134ab
Klaus Liebler <mail@klaus-liebler.de>
2024-12-03 12:51:40 +0100

    Create Aufgabe_11_02_variante.py

---
+++ 11_web/Aufgabe_11_02_variante.py
@@ -0,0 +1,18 @@
+import urllib.parse
+import urllib.request
+import json
+data = {'to':'USD'}
+url_values = urllib.parse.urlencode(data)
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-Agent', 'MyApp/1.0')]
+urllib.request.install_opener(opener)
+url = 'https://api.frankfurter.app/latest'
+full_url = url + '?' + url_values
+print("An den Server wird gesendet:", full_url)
+with urllib.request.urlopen(full_url) as response:
+    result = response.read()
+    print(result)
+    parsed_result = json.loads(result)
+    city = "US"
+    usd=parsed_result["rates"]["USD"]
+    print(f"Ein Euro ist {usd:.2f} USD")
