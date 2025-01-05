b065afc62c57d8f81f24cbcb041a039b7c68f304
Klaus Liebler <mail@klaus-liebler.de>
2025-01-05 20:34:18 +0100

    Fix deprecated warnings

--- 19_fpdf/01_beispiel.py
+++ 19_fpdf/01_beispiel.py
@@ -1,22 +1,23 @@
-# pip install fpdf
-from fpdf import FPDF
+# Achtung: Es muss fpdf2 installiert werden
+# pip install fpdf2
+from fpdf import FPDF, XPos, YPos
 #https://pyfpdf.readthedocs.io/en/latest/Tutorial/index.html
 pdf = FPDF('P', 'mm', 'A4')
 pdf.add_page()
 pdf.set_margins(0, 0, 0)
-pdf.set_font('Arial', 'B', 16)
-pdf.cell(40, 10, 'Hello World!', 1,1)
+pdf.set_font('helvetica', 'B', 16)
+pdf.cell(40, 10, 'Hello World!', 1, new_x=XPos.LMARGIN, new_y=YPos.NEXT)
 pdf.set_font('Times', '', 12)
-pdf.cell(210, 10, 'Powered by Hochschule Osnabrück.', 0, 1, 'C')
+pdf.cell(210, 10, 'Powered by Hochschule Osnabrück.', 0, new_x=XPos.LMARGIN, new_y=YPos.NEXT, align="C")
 pdf.image('./data/logo.jpg', x = 55, y = 32.5, w = 100, h = 0, type = '', link = '')
 pdf.line(x1 = 65, y1 = 27.5, x2 = 145, y2 = 27.5)
 pdf.rect(x = 55, y = 20, w = 100, h = 55, style = '')
-pdf.cell()
-pdf.cell(210, 10, 'Osnabrück ist suuuper', 0, 1, 'C')
+
+pdf.cell(210, 10, 'Osnabrück ist suuuper', 0, new_x=XPos.LMARGIN, new_y=YPos.NEXT, align="C")
 
 pdf.set_fill_color(128, 166, 197)
 pdf.set_draw_color(41, 53, 50)
 pdf.rect(80, 115, 50, 10, 'DF')
 
 
-pdf.output('sample.pdf', 'F')
+pdf.output('sample.pdf')
